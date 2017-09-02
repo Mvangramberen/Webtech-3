ca4ab97b8c10fcd79cc769420ea44693dc4a869d
Mickaël Vangramberen <m.vangramberen@hotmail.com>
2017-09-02 14:11:13 +0200

    Created student object

---
+++ JSP2/src/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+	/*
+	 * To auto generate getters and setters in Eclipse
+	 * shortcut = "ALT + SHIFT + S" 
+	 * then press "R"
+	 */
+	
+	private String firstName;
+	private String lastName;
+	private int grade;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	
+	
+}
